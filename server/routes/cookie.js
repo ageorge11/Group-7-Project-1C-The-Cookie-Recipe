656c18f1cf497d0079f07d35bf6f0e0400467d81
ageorge11 <ageorg11@confederationcollege.ca>
2020-12-13 23:18:25 -0500

    commiting some final changes

--- server/routes/cookie.js
+++ server/routes/cookie.js
@@ -1,64 +1,16 @@
-const db = require('../db');
+const router = require('express').Router();
+const cookieController = require('../controllers/cookie');
 
-exports.get = async (req, res) => {
-    console.log("get All Cookies : "+JSON.stringify(req.body));
-    cookies = await db.getAllCookie()
+router.post ('/get', cookieController.get);
 
-    res.status(200).json({
-        "cookies": cookies
-    });
+//router.post ('/comment', cookieController.comment);
+router.post ('/getRating', cookieController.getRating);
 
-}
+router.post ('/rate', cookieController.rate);
 
-exports.add = async (req, res) => {
-    console.log("Adding Cookie"+JSON.stringify(req.body));
+//router.post ('/search', cookieController.search);
 
-    const { recipe_name, recipe_desc, user_id } = req.body;
-    await db.addCookie(req.body);
-    res.status(201).json({
-        "message:": "Cookie Added"
-    });
+//cookie create 
+router.post ('/add', cookieController.add);
 
-}
-
-exports.getRating = async (req, res) => {
-   // console.log("Get Rating : "+JSON.stringify(req.body));
-    const { recipe_id, type } = req.body;
-    result1 = await db.getRating(recipe_id, "like");
-    result2 = await db.getRating(recipe_id, "dislike");
-
-    //console.log(result1);
-    //console.log(result1[0].rating);
-
-    var rating = {
-        "like": result1[0].rating,
-        "dislike": result2[0].rating
-    };
-
-    if (result1) {
-        res.status(200).json({
-            rating
-        });
-    }
-    else {
-
-    }
-    // console.log(result);
-
-}
-
-exports.comment = async (req, res) => {
-    console.log(JSON.stringify(req.body));
-
-}
-
-exports.rate = async (req, res) => {
-    console.log(JSON.stringify(req.body));
-
-}
-
-exports.search = async (req, res) => {
-    console.log(JSON.stringify(req.body));
-
-}
-
+module.exports = router;
