e65b433ff25531d878a48528cff7a6665d3abf14
arsh1401 <arshdeep.ad103@gmail.com>
2020-12-13 22:41:12 -0500

    getting routes/user.js file and routes/cookie.js file

--- server/server.js
+++ server/server.js
@@ -13,7 +13,21 @@ const app = express();
 const corsOptions = {
   credentials: true
 };
+app.use(cors(corsOptions));
 
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+
+app.use(express.static('public'));
+
+const userRoute = require('./routes/user');
+const cookieRoute = require('./routes/cookie');
+
+
+app.use('/api/user', userRoute);
+app.use('/api/cookie',cookieRoute);
+
+//console.log(Intl.DateTimeFormat().resolvedOptions().timeZone );
 
 app.listen(process.env.PORT || '3000', () => {
   console.log(`Server Running on port: ${process.env.PORT || '3000'}`);
